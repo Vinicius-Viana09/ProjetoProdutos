22b85980a9ed27c222c5e1b34f32cf4c04a077b2
caio <viniciusniviana@gmail.com>
2021-06-08 11:00:30 -0300

    login começo

--- Classes/Login.cs
+++ Classes/Login.cs
@@ -1,7 +1,42 @@
+using System;
+
 namespace gerenciamentoProdutos.Classes
 {
     public class Login
     {
-        
+
+        public bool Logado = false;
+
+        public Login()
+        {
+        }
+
+        public string Logar()
+        {
+            Usuario u = new Usuario();
+            u.Cadastrar(u);
+            Console.WriteLine("fazendo login");
+            Console.WriteLine("confirme seu email");
+            string confirmarEmail = Console.ReadLine();
+            Console.WriteLine("confirme sua senha");
+            string confirmarSenha = Console.ReadLine();
+
+            if (confirmarEmail == u.email || confirmarSenha == u.senha)
+            {
+                Logado = true;
+                // CRIAR MENU AQUI!!!!
+            }
+            else
+            {
+                Console.WriteLine("usuario ou senha incorreto");
+            }
+            return "";
+        }
+
+        public string Deslogar()
+        {
+            return "";
+
+        }
     }
 }
